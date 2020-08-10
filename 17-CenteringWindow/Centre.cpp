16d80733b6d5cdddf45911b18ddf8e9ef2141981
udayahuja <udayahuja>
2020-08-10 15:38:40 +0530

    Centering Window

---
+++ 17-CenteringWindow/Centre.cpp
@@ -0,0 +1,52 @@
+﻿#include<windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstace, LPSTR lpszCmdShow, int iCmdShow) {
+
+	MSG msg;
+	HWND hwnd;
+	TCHAR szAppName[] = TEXT("App");
+	WNDCLASSEX wndclass;
+	HDC hdc = GetWindowDC(NULL);
+	int x = GetDeviceCaps(hdc, HORZRES)/4;
+	int y = GetDeviceCaps(hdc, VERTRES)/4;
+
+	wndclass.cbSize = sizeof(wndclass);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.hCursor = LoadCursor(NULL, IDC_CROSS);
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hInstance = hInstance;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+
+	RegisterClassEx(&wndclass);
+
+	
+	hwnd = CreateWindow(szAppName, TEXT("App"), WS_OVERLAPPEDWINDOW, x, y, 2*x, 2*y, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+	
+	switch (iMsg) {
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+	}
+	return DefWindowProc(hWnd, iMsg, wParam, lParam);
+}
